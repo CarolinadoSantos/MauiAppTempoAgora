4e8a561162f1a4970847d1c370e4d30fbbda05d8
Carol Santos <carol.natalia@gmail.com>
2026-03-31 18:31:05 -0300

    app tempo agora

---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MauiAppTempoAgora.Models;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo t = null;
+
+            string chave = "8ead1b55639847212fca59bffd20e820";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+                }
+            }
+
+            return t;
+        }
+    }
+}
